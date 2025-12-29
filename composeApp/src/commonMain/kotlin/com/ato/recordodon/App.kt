c4323023e81f1f5db365323fbbe8c908e2878f32
alexander-topilskii <alexander.topilskii@gmail.com>
2025-12-29 12:48:53 +0200

    Add navigation and screen structure

--- composeApp/src/commonMain/kotlin/com/ato/recordodon/App.kt
+++ composeApp/src/commonMain/kotlin/com/ato/recordodon/App.kt
@@ -1,48 +1,42 @@
 package com.ato.recordodon
 
-import androidx.compose.animation.AnimatedVisibility
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.safeContentPadding
-import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
-import androidx.compose.runtime.*
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import org.jetbrains.compose.resources.painterResource
+import androidx.compose.runtime.Composable
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.ato.recordodon.navigation.AppRoutes
+import com.ato.recordodon.screens.about.AboutScreen
+import com.ato.recordodon.screens.main.MainScreen
+import com.ato.recordodon.screens.recorder.RecorderScreen
+import com.ato.recordodon.screens.settings.SettingsScreen
 import org.jetbrains.compose.ui.tooling.preview.Preview
-
-import recordodon.composeapp.generated.resources.Res
-import recordodon.composeapp.generated.resources.compose_multiplatform
 
 @Composable
 @Preview
 fun App() {
     MaterialTheme {
-        var showContent by remember { mutableStateOf(false) }
-        Column(
-            modifier = Modifier
-                .background(MaterialTheme.colorScheme.primaryContainer)
-                .safeContentPadding()
-                .fillMaxSize(),
-            horizontalAlignment = Alignment.CenterHorizontally,
+        val navController = rememberNavController()
+        NavHost(
+            navController = navController,
+            startDestination = AppRoutes.MAIN,
         ) {
-            Button(onClick = { showContent = !showContent }) {
-                Text("Click me!")
+            composable(AppRoutes.MAIN) {
+                MainScreen(
+                    recordings = emptyList(),
+                    onCreateRecording = { navController.navigate(AppRoutes.RECORDER) },
+                    onOpenSettings = { navController.navigate(AppRoutes.SETTINGS) },
+                    onOpenAbout = { navController.navigate(AppRoutes.ABOUT) },
+                )
             }
-            AnimatedVisibility(showContent) {
-                val greeting = remember { Greeting().greet() }
-                Column(
-                    modifier = Modifier.fillMaxWidth(),
-                    horizontalAlignment = Alignment.CenterHorizontally,
-                ) {
-                    Image(painterResource(Res.drawable.compose_multiplatform), null)
-                    Text("Compose: $greeting")
-                }
+            composable(AppRoutes.SETTINGS) {
+                SettingsScreen(onBack = { navController.popBackStack() })
+            }
+            composable(AppRoutes.ABOUT) {
+                AboutScreen(onBack = { navController.popBackStack() })
+            }
+            composable(AppRoutes.RECORDER) {
+                RecorderScreen(onBack = { navController.popBackStack() })
             }
         }
     }
